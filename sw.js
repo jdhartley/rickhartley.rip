891e8b697eccec25ea418330fd87238e0bf7c376
JD Hartley <jdhrtl@gmail.com>
2018-09-22 14:39:00 -0700

    birthday easter egg

--- sw.js
+++ sw.js
@@ -36,8 +36,8 @@ const RUNTIME_BLACKLIST = [
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v3';
-const RUNTIME = 'runtime-v3';
+const PRECACHE = 'precache-v4';
+const RUNTIME = 'runtime-v4';
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
